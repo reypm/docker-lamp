140b3fd71f709c27d6693da8bd582f683f9a751d
Reynier Perez <rperez@managedmaint.com>
2017-05-17 11:25:21 -0400

    Fixed incompatibilities issues with OSX

--- config/entrypoint.sh
+++ config/entrypoint.sh
@@ -3,24 +3,20 @@ set -e
 
 # Setup permissions
 data_dir="/var/www/html"
-composer_cache_dir="/var/www/.composer"
 
-mkdir -p "$composer_cache_dir"
+if [ "$(id -u)" == 1000 ]; then
+    usermod -u 1000 www-data && groupmod -g 1000 www-data
+elif [ "$(id -u)" == 500 ]; then
+    usermod -u 500 www-data && groupmod -g 500 www-data
+fi
 
-usermod -u 1000 www-data && groupmod -g 1000 www-data
 chown -R www-data:root "$data_dir"
-chown -R www-data:root "$composer_cache_dir"
 
 if  [ -d "$data_dir" ]; then
     chgrp -R www-data "$data_dir"
     chmod -R g+w "$data_dir"
     find "$data_dir" -type d -exec chmod 2775 {} +
     find "$data_dir" -type f -exec chmod ug+rw {} +
-fi
-
-if  [ -d "$composer_cache_dir" ]; then
-    chgrp -R www-data "$composer_cache_dir"
-    chmod -R g+w "$composer_cache_dir"
 fi
 
 # Enable rewrite
