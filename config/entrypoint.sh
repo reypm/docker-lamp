4290b672964c9e09d6190e7d43e1b804f36cf10e
Reynier Perez <rperez@managedmaint.com>
2017-05-17 15:14:42 -0400

    Fixing OSX permissions

--- config/entrypoint.sh
+++ config/entrypoint.sh
@@ -4,16 +4,12 @@ set -e
 # Setup permissions
 data_dir="/var/www/html"
 
-if [ "$(id -u)" == 1000 ]; then
-    usermod -u 1000 www-data && groupmod -g 1000 www-data
-elif [ "$(id -u)" == 500 ]; then
-    usermod -u 500 www-data && groupmod -g 500 www-data
-fi
+usermod -u 1000 www-data && groupmod -g 1000 www-data
 
 chown -R www-data:root "$data_dir"
 
 if  [ -d "$data_dir" ]; then
-    chgrp -R www-data "$data_dir"
+    chgrp -RH www-data "$data_dir"
     chmod -R g+w "$data_dir"
     find "$data_dir" -type d -exec chmod 2775 {} +
     find "$data_dir" -type f -exec chmod ug+rw {} +
